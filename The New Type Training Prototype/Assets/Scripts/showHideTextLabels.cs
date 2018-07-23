f1fb67f887fd4c9453335d30270bb302099fe964
Maxim Molenaars <maximmolenaars@gmail.com>
2018-07-23 13:21:43 +0200

    before merge with 1.5ARKit

---
+++ The New Type Training Prototype/Assets/Scripts/showHideTextLabels.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showHideTextLabels : MonoBehaviour {
+
+    GameObject textLabels;
+    public Button m_YourFirstButton, m_YourSecondButton;
+
+   	// Use this for initialization
+	void Start () {
+
+        textLabels = GameObject.Find("/HitCubeParent/TextLabels");
+        FindButtonsInScene();
+
+       
+
+
+	}
+
+    void FindButtonsInScene(){
+        Button btn1 = m_YourFirstButton.GetComponent<Button>();
+        Button btn2 = m_YourSecondButton.GetComponent<Button>();
+        btn1.onClick.AddListener(TaskOnClick);
+        btn2.onClick.AddListener(TaskOnClick2);
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+        textLabels.SetActive(false);
+    }
+
+    void TaskOnClick2()
+    {
+        //Output this to console when the Button is clicked
+        Debug.Log("You have clicked the button!");
+        textLabels.SetActive(true);
+    }
+	
+
+}
