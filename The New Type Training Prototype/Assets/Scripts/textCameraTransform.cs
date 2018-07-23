0cd1caa1314fa94c5e631fcde663ead31d76ae52
Maxim Molenaars <maximmolenaars@gmail.com>
2018-07-23 11:29:26 +0200

    added textmesh and textFollowCamera script

---
+++ The New Type Training Prototype/Assets/Scripts/textCameraTransform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class textCameraTransform : MonoBehaviour {
+
+    public Camera cameraToFollow;
+
+	// Update is called once per frame
+	void Update () {
+
+        transform.LookAt(transform.position + cameraToFollow.transform.rotation * Vector3.zero,
+                         cameraToFollow.transform.rotation * Vector3.down);
+		
+	}
+}
